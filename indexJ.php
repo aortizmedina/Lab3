1a95c3671b1f3ee0dd76d4fe65548963e8383083
Brittany Arnold <barnold@csumb.edu>
2017-09-24 19:35:25 +0000

    Fixed names next to players img

--- indexJ.php
+++ indexJ.php
@@ -6,177 +6,139 @@
 		<link href="css/styles.css" rel="stylesheet" type="text/css">
 	</head>
     <body>
-        
+        <h1>SILVERJACK</h1>
         <?php 
-         global $deck;
+            global $deck;
         
-        // Generate a deck of cards 
-        // [0, 1, 2, ..., 51]
-        // map each number to a card 
-        
-        // generate a "hand" of cards
-        
-        function mapNumberToCard($num) {
-            $cardValue = ($num % 13) + 1; 
-            $cardSuit = floor($num / 13); 
-            $suitStr = ""; 
-            
-            switch($cardSuit) {
-                case 0: 
-                    $suitStr = "clubs"; 
-                    break; 
-                case 1: 
-                    $suitStr = "diamonds"; 
-                    break; 
-                case 2: 
-                    $suitStr = "hearts"; 
-                    break; 
-                case 3: 
-                    $suitStr = "spades"; 
-                    break; 
-            }
+            function mapNumberToCard($num) {
+                $cardValue = ($num % 13) + 1; 
+                $cardSuit = floor($num / 13); 
+                $suitStr = ""; 
+                switch($cardSuit) {
+                    case 0: 
+                        $suitStr = "clubs"; 
+                        break; 
+                    case 1: 
+                        $suitStr = "diamonds"; 
+                        break; 
+                    case 2: 
+                        $suitStr = "hearts"; 
+                        break; 
+                    case 3: 
+                        $suitStr = "spades"; 
+                        break; 
+                }
 
-            $card = array(
-                'num' => $cardValue, 
-                'suit' => $cardSuit, 
-                'imgURL' => "./cards/".$suitStr."/".$cardValue.".png"
-                ); 
-            
-            return $card; 
-        }
-        
-        
-        function generateDeck() {
-            $cards = array(); 
-        
-            for ($i = 0; $i < 52; $i++) {
-                array_push($cards, $i); 
+                $card = array(
+                    'num' => $cardValue, 
+                    'suit' => $cardSuit, 
+                    'imgURL' => "./cards/".$suitStr."/".$cardValue.".png"
+                    ); 
+                return $card; 
             }
-            
-            
-            shuffle($cards); 
-            
-            return $cards; 
- 
-        }
         
+            function generateDeck() {
+                $cards = array(); 
+                for ($i = 0; $i < 52; $i++) {
+                    array_push($cards, $i); 
+                }
+                shuffle($cards); 
+                return $cards; 
+            }
         
-        
-               function calculateHandValue($cards) {
+            function calculateHandValue($cards) {
                 $sum = 0; 
-                
                 foreach ($cards as $card) { // code gives me an error here!!
                     $sum += $card["num"]; 
                 }
-                
                 return $sum; 
             }
-               
-           
-            
-        // Return a specific number of cards
         
-        // function that generates a "hand" of cards for one person (no duplicates)
-        function generateHand() {
-            $hand = array(); 
-             global $deck;
-          do {
-                $cardNum = array_pop($deck);
-                $card = mapNumberToCard($cardNum); 
-                array_push($hand, $card); 
-               } while (calculateHandValue($hand)<36);
-            
-            return $hand; 
-        }
+            // function that generates a "hand" of cards for one person (no duplicates)
+            function generateHand() {
+                $hand = array(); 
+                global $deck;
+                do {
+                    $cardNum = array_pop($deck);
+                    $card = mapNumberToCard($cardNum); 
+                    array_push($hand, $card); 
+                } while (calculateHandValue($hand)<36);
+                return $hand; 
+            }
         
-        $deck = generateDeck(); 
-        
-         function players() {
-            $playerPic = rand(1,13); 
-            $playerName = ""; 
-            $randName = rand(1,13);
+            function players() {
+                $playerPic = rand(1, 13); 
+                $playerName = ""; 
+                $randName = rand(1, 13);
             
-            switch($randName) {
-                case 0: 
-                    $playerName = "Butters"; 
-                    break; 
-                case 1: 
-                    $playerName = "Professor Chaos"; 
-                    break; 
-                case 2: 
-                    $playerName = "Ike"; 
-                    break; 
-                case 3: 
-                    $playerName = "Jesus"; 
-                    break; 
-                case 4: 
-                    $playerName = "Bebe"; 
-                    break; 
-                case 5: 
-                    $playerName = "Pete"; 
-                    break; 
-                case 6:
-                    $playerName = "Cartman"; 
-                    break; 
-                case 7:
-                    $playerName = "Jimmy"; 
-                    break; 
-                case 8:
-                    $playerName = "Kenny"; 
-                    break; 
-                case 9:
-                    $playerName = "Kyle"; 
-                    break; 
-                case 10:
-                    $playerName = "Stan"; 
-                    break; 
-                case 11:
-                    $playerName = "Randy"; 
-                    break; 
-                case 12:
-                    $playerName = "Santa"; 
-                    break; 
-                
+                switch($randName) {
+                    case 0: 
+                        $playerName = "Butters"; 
+                        break; 
+                    case 1: 
+                        $playerName = "Professor Chaos"; 
+                        break; 
+                    case 2: 
+                        $playerName = "Ike"; 
+                        break; 
+                    case 3: 
+                        $playerName = "Jesus"; 
+                        break; 
+                    case 4: 
+                        $playerName = "Bebe"; 
+                        break; 
+                    case 5: 
+                        $playerName = "Pete"; 
+                        break; 
+                    case 6:
+                        $playerName = "Cartman"; 
+                        break; 
+                    case 7:
+                        $playerName = "Jimmy"; 
+                        break; 
+                    case 8:
+                        $playerName = "Kenny"; 
+                        break; 
+                    case 9:
+                        $playerName = "Kyle"; 
+                        break; 
+                    case 10:
+                        $playerName = "Stan"; 
+                        break; 
+                    case 11:
+                        $playerName = "Randy"; 
+                        break; 
+                    case 12:
+                        $playerName = "Santa"; 
+                        break; 
+                }
+                $profile = array(
+                    'name' => $playerName, 
+                    'imgURL' => "./profile_pics/".$playerPic.".png"
+                ); 
+                return $profile; 
             }
 
-            $profile = array(
-                'name' => $playerName, 
-                'imgURL' => "./profile_pics/".$playerPic.".png"
-                ); 
-            
-            return $profile; 
-        }
-        
-        for($i=1;$i<5;$i++)
-        {
-         $p["$i"]=players();
-         $person["$i"] = array(
-            "name" => $p["$i"]["name"], 
-            "profilePicUrl" => $p["$i"]["imgURL"], 
-            "cards" => generateHand($deck)
-            ); 
-             displayPerson($person["$i"]); 
-        }
-            //const linebreak = "<br/>";
-            
             function displayPerson($person) {
-                // show profile pic
-                echo "<img src='".$person["profilePicUrl"]."'>"; 
-                
-                
-                // iterate through $person's "cards"
-                
-                for($i = 0; $i < count($person["cards"]); $i++) {
+                echo "<img src='".$person["profilePicUrl"]."'>"; // show profile pic
+                echo $person["name"];
+                for($i = 0; $i < count($person["cards"]); $i++) { // iterate through $person's "cards"
                     $card = $person["cards"][$i];
-                    
-                    // construct the imgURL for each card
-                    
-                    // translate this to HTML 
-                    echo "<img src='".$card["imgURL"]."'>"; 
+                    echo "<img src='".$card["imgURL"]."'>"; // construct the imgURL for each card and translate this to HTML 
                 }
-                
-                echo calculateHandValue($person["cards"]);
-                echo $person["name"].'<br>';
+                echo calculateHandValue($person["cards"]).'<br>';
+            }
+            
+            $deck = generateDeck(); 
+            
+            for($i = 1; $i < 5; $i++) {
+                $p["$i"]=players();
+                $person["$i"] = array(
+                "name" => $p["$i"]["name"], 
+                "profilePicUrl" => $p["$i"]["imgURL"], 
+                "cards" => generateHand($deck)
+            ); 
+            displayPerson($person["$i"]); 
             }
             
             $score1 = calculateHandValue($person1["cards"]);
@@ -265,5 +227,7 @@
             
         
         ?>
+        
+        
     </body>
 </html>
